909d6e55217acbeb1fac16a4a060bf046fc540ff
jrue moolifee <simoonhy@126.com>
2016-10-22 19:27:11 +0800

    修改配置文件

--- config.php
+++ config.php
@@ -5,4 +5,5 @@ return [
 	'accessKeyId'     => '创建moolifee分支',
 	'accessKeySecret' => 'Ml7EeZo9lspICHqRyACPGTBO6OjSFp',
 	'accessHost'      => 'http://www.aikena.com/',
+	'author'          => 'Jrue Moolifee',
 ];
