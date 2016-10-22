c10e60e7242f22d63fd05d0b64932dd64a452ed2
jrue moolifee <simoonhy@126.com>
2016-10-22 14:27:20 +0800

    公共文件
    Signed-off-by: jrue moolifee <simoonhy@126.com>

---
+++ common.php
@@ -0,0 +1,27 @@
+<?php
+# 公共文件，项目共用代码
+
+class Common
+{
+    const endpoint = Config::OSS_ENDPOINT;
+    const accessKeyId = Config::OSS_ACCESS_ID;
+    const accessKeySecret = Config::OSS_ACCESS_KEY;
+    const bucket = Config::OSS_TEST_BUCKET;
+
+    /**
+     * 根据Config配置，得到一个OssClient实例
+     *
+     * @return OssClient 一个OssClient实例
+     */
+    public static function getOssClient()
+    {
+        try {
+            $ossClient = new OssClient(self::accessKeyId, self::accessKeySecret, self::endpoint, true);
+        } catch (OssException $e) {
+            printf(__FUNCTION__ . "creating OssClient instance: FAILED\n");
+            printf($e->getMessage() . "\n");
+            return null;
+        }
+        return $ossClient;
+    }
+}
